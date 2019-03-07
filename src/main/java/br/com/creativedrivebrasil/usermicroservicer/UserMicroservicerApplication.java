eb3228f9a001fb407113d40ac7598a943a3f2462
Tomasoares <tomasoares@gmail.com>
2019-03-07 19:14:11 -0300

    first commit
    first commit

---
+++ src/main/java/br/com/creativedrivebrasil/usermicroservicer/UserMicroservicerApplication.java
@@ -0,0 +1,13 @@
+package br.com.creativedrivebrasil.usermicroservicer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMicroservicerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMicroservicerApplication.class, args);
+	}
+
+}
