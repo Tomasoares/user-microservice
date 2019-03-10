1b4db32ef2c30fe81b17d55757371498698c10ef
Tomasoares <tomasoares@gmail.com>
2019-03-10 17:50:12 -0300

    add creativedriver default user
    add creativedriver default user

--- volumes/mongo/docker-entrypoint-initdb.d/create-user.js
+++ volumes/mongo/docker-entrypoint-initdb.d/create-user.js
@@ -1,4 +1,4 @@
-db.getSiblingDB('user-microservice');
+db.getSiblingDB('user-microservice')
 
 db.createUser(
 	{
@@ -6,4 +6,15 @@ db.createUser(
 		pwd: "cr34t1v3dr1v3r",
 		roles: [ { role: "readWrite", db: "user-microservice" } ]
 	}
-);
+)
+
+db.users.insert(
+	{
+		"name": "Creative Driver",
+		"email": "creativedriver@gmail.com",
+		"password": "cr34t1v3dr1v3r",
+		"address": "SC-401",
+		"telephone": "37468533467",
+		"idUserType": 1
+	}
+)
